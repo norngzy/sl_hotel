c1dd9090a769e77e3dc702262bd223ea1442e6af
norngzy <yoonjeero@gmail.com>
2026-04-16 19:12:03 +0900

    객실,다이닝css추가

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,19 @@
 const heroBnr = document.querySelector('.hero');
-console.log(heroBnr);
+const roomSlide = document.querySelector('.room_swiper');
+console.log(heroBnr,roomSlide);
 
 const heroSwiper = new Swiper(heroBnr,{
     loop:true,
     navigation:{
-    prevEl:'.hero .prev',
-    nextEl:'.hero .next',
+        prevEl:'.hero .prev',
+        nextEl:'.hero .next',
+    },
+});
+
+const roomSwiper = new Swiper(roomSlide,{
+    loop:true,
+    navigation:{
+        prevEl:'.room_swiper .prev',
+        nextEl:'.room_swiper .next',
     },
 });
