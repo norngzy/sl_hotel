94275c6f8df8eb907c16a4fac8925c10c7695509
norngzy <yoonjeero@gmail.com>
2026-04-17 20:22:47 +0900

    css완성!!

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,9 @@
 const heroBnr = document.querySelector('.hero');
 const roomSlide = document.querySelector('.room_swiper');
-console.log(heroBnr,roomSlide);
+const offerSlide = document.querySelector('.offer_swiper');
+const diningMain = document.querySelectorAll('.dining_main_tap a');
+const diningContents = document.querySelectorAll('.dining_contents #mainslide');
+console.log(heroBnr,roomSlide,offerSlide,diningMain,diningContents);
 
 const heroSwiper = new Swiper(heroBnr,{
     loop:true,
@@ -17,3 +20,45 @@ const roomSwiper = new Swiper(roomSlide,{
         nextEl:'.room_swiper .next',
     },
 });
+
+const offerSwiper = new Swiper(offerSlide,{
+    loop:true,
+    slidesPerView:3,
+    slidesPerGroup:3,
+    pagination:{
+        el:'.offer_swiper .progressbar',
+        type:'progressbar',
+    },
+});
+
+//1.라운지,베이커리숨기기 
+diningContents[1].style.display ='none';
+diningContents[2].style.display ='none';
+// 2. 탭제목 클릭시 변경 
+diningMain[0].addEventListener('click',function(e){
+    e.preventDefault();
+    diningMain[0].classList.add('active');
+    diningMain[1].classList.remove('active');
+    diningMain[2].classList.remove('active');
+    diningContents[0].style.display ='block';
+    diningContents[1].style.display ='none';
+    diningContents[2].style.display ='none';
+})
+diningMain[1].addEventListener('click',function(e){
+    e.preventDefault();
+    diningMain[1].classList.add('active');
+    diningMain[0].classList.remove('active');
+    diningMain[2].classList.remove('active');
+    diningContents[1].style.display ='block';
+    diningContents[0].style.display ='none';
+    diningContents[2].style.display ='none';
+})
+diningMain[2].addEventListener('click',function(e){
+    e.preventDefault();
+    diningMain[2].classList.add('active');
+    diningMain[0].classList.remove('active');
+    diningMain[1].classList.remove('active');
+    diningContents[2].style.display ='block';
+    diningContents[0].style.display ='none';
+    diningContents[1].style.display ='none';
+})
